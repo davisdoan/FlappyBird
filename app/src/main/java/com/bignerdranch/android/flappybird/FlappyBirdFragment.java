9028e853fcfe6c0f0b1f4df1810124f94beb09a8
Davis Doan <davisdoan@gmail.com>
2016-03-01 00:07:09 -0800

    Changed Bird to PNG with transparent background. Added in onTouch Logic.

--- app/src/main/java/com/bignerdranch/android/flappybird/FlappyBirdFragment.java
+++ app/src/main/java/com/bignerdranch/android/flappybird/FlappyBirdFragment.java
@@ -6,8 +6,10 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 
 public class FlappyBirdFragment extends Fragment{
@@ -15,11 +17,9 @@ public class FlappyBirdFragment extends Fragment{
     private View mFloorView;
     private View mGrassView;
     private View mSceneView;
-    private View mDrawingView;
     private View mTopPipe;
     private View mBottomPipe;
     private AnimationDrawable birdAnimation;
-    private AnimationDrawable birdAnim2;
     private boolean mPipeFlag;
 
     public static FlappyBirdFragment newInstance() { return new FlappyBirdFragment();}
@@ -28,7 +28,7 @@ public class FlappyBirdFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle onSavedInstanceState){
         View view = inflater.inflate(R.layout.flappybird_fragment, container, false);
 
-        ImageView mBirdScene = (ImageView) view.findViewById(R.id.flappy_bird);
+        final ImageView mBirdScene = (ImageView) view.findViewById(R.id.flappy_bird);
         this.birdAnimation = (AnimationDrawable) mBirdScene.getBackground();
         birdAnimation.start();
 
@@ -40,18 +40,43 @@ public class FlappyBirdFragment extends Fragment{
         mTopPipe = view.findViewById(R.id.top_pipe);
         mBottomPipe = view.findViewById(R.id.bottom_pipe);
 
+
+        mSceneView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                AnimatorSet animatorSet = new AnimatorSet();
+                ObjectAnimator birdDownAnimator = ObjectAnimator.ofFloat(mBirdScene, "y", mBirdScene.getHeight(), -3).setDuration(30);
+                ObjectAnimator birdUpAnimator = ObjectAnimator.ofFloat(mBirdScene, "y", v.getBottom(), v.getTop()).setDuration(30);
+                birdUpAnimator.setInterpolator(new AccelerateInterpolator());
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        animatorSet.play(birdDownAnimator);
+                        animatorSet.start();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        animatorSet.play(birdDownAnimator);
+                        animatorSet.start();
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        break;
+
+                }
+                return true;
+            }
+        });
+
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mPipeFlag == false) {
+                if (mPipeFlag == false) {
                     startPipeAnimation();
                 }
             }
         });
-
-        ImageView mBirdTwo = (ImageView) view.findViewById(R.id.flappy_bird2);
-        this.birdAnim2 = (AnimationDrawable) mBirdTwo.getBackground();
-        birdAnim2.start();
 
         return view;
     }
@@ -60,8 +85,8 @@ public class FlappyBirdFragment extends Fragment{
         float topPipeXStart = mTopPipe.getX();
         float bottomPipeXStart = mBottomPipe.getX();
 
-        ObjectAnimator bottomWidthAnimator = ObjectAnimator.ofFloat(mTopPipe, "x", topPipeXStart,0).setDuration(3000);
-        ObjectAnimator topWidthAnimator = ObjectAnimator.ofFloat(mBottomPipe, "x", bottomPipeXStart,0).setDuration(3000);
+        ObjectAnimator bottomWidthAnimator = ObjectAnimator.ofFloat(mTopPipe, "x", topPipeXStart,-250).setDuration(3000);
+        ObjectAnimator topWidthAnimator = ObjectAnimator.ofFloat(mBottomPipe, "x", bottomPipeXStart,-250).setDuration(3000);
 
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(bottomWidthAnimator).with(topWidthAnimator);
