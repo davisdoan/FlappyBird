07257db254bcb645f38596e4117ed561a8eb174a
Davis Doan <davisdoan@gmail.com>
2016-03-02 00:33:50 -0800

    Added in onClick logic to move the bird up and a to make the bird fall after the flup up is completed.

--- app/src/main/java/com/bignerdranch/android/flappybird/FlappyBirdFragment.java
+++ app/src/main/java/com/bignerdranch/android/flappybird/FlappyBirdFragment.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
@@ -21,15 +20,21 @@ public class FlappyBirdFragment extends Fragment{
     private View mSceneView;
     private View mTopPipe;
     private View mBottomPipe;
+    private View mBirdScene;
     private AnimationDrawable birdAnimation;
+    private int startingSpot;
     private boolean mPipeFlag;
+    private boolean mFlyFlag;
+    private ObjectAnimator birdUpAnimator;
+    private ObjectAnimator birdDownAnimator;
+
     public static FlappyBirdFragment newInstance() { return new FlappyBirdFragment();}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle onSavedInstanceState){
         View view = inflater.inflate(R.layout.flappybird_fragment, container, false);
 
-        final ImageView mBirdScene = (ImageView) view.findViewById(R.id.flappy_bird);
+        mBirdScene = (ImageView) view.findViewById(R.id.flappy_bird);
         this.birdAnimation = (AnimationDrawable) mBirdScene.getBackground();
         birdAnimation.start();
 
@@ -41,16 +46,63 @@ public class FlappyBirdFragment extends Fragment{
         mTopPipe = view.findViewById(R.id.top_pipe);
         mBottomPipe = view.findViewById(R.id.bottom_pipe);
 
+        birdUpAnimator = ObjectAnimator.ofFloat(mBirdScene, "translationY", -200);
+        birdUpAnimator.setDuration(250);
+        birdUpAnimator.setInterpolator(new DecelerateInterpolator());
+        birdUpAnimator.addListener(flyUpListener);
+
+        birdDownAnimator = ObjectAnimator.ofFloat(mBirdScene, "translationY", 0);
+        birdDownAnimator.setDuration(250);
+        birdDownAnimator.setInterpolator(new AccelerateInterpolator());
+
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mPipeFlag == false) {
                     startPipeAnimation();
                 }
+                if(birdUpAnimator.isStarted()){
+                    birdUpAnimator.cancel();
+                }
+
+                if(startingSpot == 0) {
+                    startingSpot = mBirdScene.getTop();
+                }
+
+                mBirdScene.layout(mBirdScene.getLeft(), (int) mBirdScene.getY(), mBirdScene.getRight(), (int) mBirdScene.getY() + mBirdScene.getMeasuredHeight());
+                mBirdScene.setTranslationY(0);
+                mFlyFlag = false;
+                birdUpAnimator.start();
             }
         });
         return view;
     }
+
+    private Animator.AnimatorListener flyUpListener = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            if(!mFlyFlag){
+                mBirdScene.layout(mBirdScene.getLeft(),(int)mBirdScene.getY(), mBirdScene.getRight(), (int)mBirdScene.getY() + mBirdScene.getMeasuredHeight());
+                mBirdScene.setTranslationY(0);
+                birdDownAnimator.setFloatValues(startingSpot - mBirdScene.getTop());
+                birdDownAnimator.start();
+            }
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+            mFlyFlag = true;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+    };
+
 
     private void startPipeAnimation() {
             float topPipeXStart = mTopPipe.getX();
@@ -64,29 +116,9 @@ public class FlappyBirdFragment extends Fragment{
 
             AnimatorSet animatorSet = new AnimatorSet();
             animatorSet.play(bottomWidthAnimator).with(topWidthAnimator);
-
+            mPipeFlag = true;
             animatorSet.start();
     }
 
-    private Animator.AnimatorListener flyUpListener = new Animator.AnimatorListener() {
-        @Override
-        public void onAnimationStart(Animator animation) {
 
-        }
-
-        @Override
-        public void onAnimationEnd(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationCancel(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationRepeat(Animator animation) {
-
-        }
-    };
 }
